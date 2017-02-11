9b37d4e70e213b1023c42ebfa403ba19f619074e
leeb2018 <lukelee92@gmail.com>
2017-02-11 11:29:40 -0500

    basic structure set up done

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function (req, res) {
+	res.render("main");
+})
+
+app.listen(3000, function () {
+  console.log('App listening on port 3000!');
+})
